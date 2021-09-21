aa4900452a518180f613f41cb625dbba7c50bf32
baobaosieudebug <baobaosieudebug@gmail.com>
2021-09-22 01:16:10 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   devServer: {
       port: 8080
-  }
+  },
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/client-beta/' // Thay tên repository của các bạn vào đây nhé
+        : '/'
 }
